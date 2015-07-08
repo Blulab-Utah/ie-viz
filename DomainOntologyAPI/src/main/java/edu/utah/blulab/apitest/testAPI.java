55d3b6e71dc2e0dffd1d986cd91e582fb9705a4d
Lalindra <lalindra84@gmail.com>
2015-07-08 09:05:48 -0600

    Added sample owl file to resources and tested testAPI.java

--- DomainOntologyAPI/src/main/java/edu/utah/blulab/apitest/testAPI.java
+++ DomainOntologyAPI/src/main/java/edu/utah/blulab/apitest/testAPI.java
@@ -11,9 +11,9 @@ public class testAPI {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		//DomainOntology domain = new DomainOntology("/Users/mtharp/use_cases/DomainOntologies/pneumonia.owl");
-		DomainOntology domain = new DomainOntology("/Users/mtharp/Desktop/vincipneu.owl.xml");
+		//DomainOntology domain = new DomainOntology("/Users/mtharp/Desktop/vincipneu.owl.xml");
 		//DomainOntology domain = new DomainOntology("C:\\Users\\Bill\\Desktop\\carotid stenosis.owl"); 
-
+		DomainOntology domain = new DomainOntology("DomainOntologyAPI/src/main/resources/colonoscopy_20141001.owl");
 		//domain.getVariable("leukocytosis");
 		//domain.getVariable("KA247");
 		
