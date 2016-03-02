fc1ef73e288d523c0775494c31a4d96366a6f654
Melissa Castine <m_castine@me.com>
2016-03-02 10:27:07 -0500

    Added LogicExpression object with constructors, get/set Type methods, and boolean type methods.

---
+++ src/main/java/edu/utah/blulab/domainontology/LogicExpression.java
@@ -0,0 +1,59 @@
+package edu.utah.blulab.domainontology;
+
+import java.util.ArrayList;
+
+public class LogicExpression extends ArrayList {
+	private String type;
+	public final String AND = "AND";
+	public final String OR = "OR";
+	public final String COMPLEMENT = "COMPLEMENT";
+	public final String SINGLE = "SINGLE";
+	
+	public LogicExpression(String type){
+		this.type = type;
+	}
+	
+	public LogicExpression(){
+		type = "";
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	public boolean isAndExpression(){
+		if(type.equalsIgnoreCase(AND)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean isOrExpression(){
+		if(type.equalsIgnoreCase(OR)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean isComplementExpression(){
+		if(type.equalsIgnoreCase(COMPLEMENT)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean isSingleExpression(){
+		if(type.equalsIgnoreCase(SINGLE)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
