865ef88ce00bae814112c0074fec3f6dbde37d0d
Bill Scuba <billscuba@hotmail.com>
2016-09-21 10:45:24 -0700

    created some commmand line options with stub methods

--- src/main/java/edu/utah/blulab/commandline/IevizCmd.java
+++ src/main/java/edu/utah/blulab/commandline/IevizCmd.java
@@ -2,32 +2,55 @@ package edu.utah.blulab.commandline;
 
 import org.apache.commons.cli.*;
 
-
 /**
- * Created by Bill on 9/20/2016.
+ * Created by Bill Scuba on 9/20/2016.
  */
 public class IevizCmd {
 
     public static void main(String[] args) {
         Options options = new Options();
-        options.addOption("myCmd", "myCommand", false, "will run myCommand()." );
-        options.addOption("helloW", "helloWorld", true, "display hello word the number of time specify." );
+        options.addOption("h", "help", false, "print this message" );
+        options.addOption("ont", "ontologies", false, "view a list of available ontologies" );
+        options.addOption("wf", "workflows", false, "provides a list of available NLP workflows.");
+        options.addOption("eval", "evaluationTool", true, "specify which evaluation tool to output the results to");
+        OptionBuilder builder = OptionBuilder.hasArgs(3).withArgName("ontology> <documents> <workflow");
+        builder.withLongOpt("runieviz");
+        builder.withDescription("runs ieviz");
+        options.addOption(builder.create("run"));
 
-        System.out.println("1st Argument: " + args[0]);
+        System.out.print("Arguments:");
+        for (String arg : args) {
+            System.out.print(" " + arg);
+        }
+        System.out.print("\n\n");
 
         try{
             CommandLine line = new BasicParser().parse( options, args );
 
-            if( line.hasOption( "myCommand" ) ) {
-                myCommand();
+            if(line.hasOption("run")){
+                System.out.println("In run");
+                String[] optVals = line.getOptionValues("run");
+                for (String val : optVals){
+                    System.out.println(val);
+                }
+                run(optVals);
             }
 
-            if(line.hasOption("helloWorld")){
-                String repeat = line.getOptionValue("helloWorld");
-                Integer repeatInt = new Integer(repeat);
-                for(int i =0; i<repeatInt; i++){
-                    System.out.println( "Hello world !");
-                }
+            if(line.hasOption("help")){
+                help(options);
+            }
+
+            if(line.hasOption("ontologies")){
+                ontologies();
+            }
+
+            if(line.hasOption("workflows")){
+                workflows();
+            }
+
+            if(line.hasOption("evaluationTool")){
+                String optVal = line.getOptionValue("evaluationTool");
+                evaluationTool(optVal);
             }
 
         }catch( ParseException exp ) {
@@ -35,7 +58,20 @@ public class IevizCmd {
         }
     }
 
-    public static void myCommand(){
-        System.out.println("myCommand() just got called");
+    private static void run(String[] optVals) {
+    }
+
+    private static void ontologies() {
+    }
+
+    private static void workflows() {
+    }
+
+    private static void evaluationTool(String optVal) {
+    }
+
+    public static void help(Options options){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp( "help", options );
     }
 }
