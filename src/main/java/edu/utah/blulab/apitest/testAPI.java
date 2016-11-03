a52367eb1d74ae37bd3c693fac580dee4a34d46e
Melissa Castine <m_castine@me.com>
2016-11-03 11:31:31 -0400

    Added argument structure so that you can pass the location of owl file and boolean value using command line arguments.

--- src/main/java/edu/utah/blulab/apitest/testAPI.java
+++ src/main/java/edu/utah/blulab/apitest/testAPI.java
@@ -11,19 +11,13 @@ public class testAPI {
 
 	public static void main(String[] args) throws Exception {
 		//DomainOntology domain = new DomainOntology("/Users/melissa/git/useCases/98_heartFailure.owl", false);
-		DomainOntology domain = new DomainOntology("/Users/melissa/git/useCases/reasonForExam.owl", false);
-		//DomainOntology domain = new DomainOntology("/Users/melissa/Desktop/demo.owl", false);
-
-		//domain.getVariable("leukocytosis");
-		//System.out.println(domain.getVariable("KA001014"));
-		//System.out.println(domain.getVariable("KA_1006"));
-		//System.out.println(domain.getVariable("KA_1003"));
-		
-		//System.out.println(domain.getVariable("KA_1005").hasNumericModifiers());
-		//System.out.println(domain.getVariable("KA_1006").hasNumericModifiers());
+		boolean bool = false;
+		if(args[1].equalsIgnoreCase("true")){
+			bool = true;
+		}
+		DomainOntology domain = new DomainOntology(args[0], bool);
 		
 		ArrayList<Variable> domainVariables = domain.getAllVariables();
-		//ArrayList<Variable> domainVariables = domain.getAllEvents();
 		System.out.println("********** Domain Variables: **********");
 		for(Variable var : domainVariables){
 			System.out.println(var.toString());
@@ -35,7 +29,7 @@ public class testAPI {
 			System.out.println(modifier.toString());
 		}
 		
-		/**System.out.println("********** Anchor Dictionary: **********");
+		System.out.println("********** Anchor Dictionary: **********");
 		ArrayList<Term> anchorDictionary = domain.createAnchorDictionary();
 		for(Term term : anchorDictionary){
 			System.out.println(term.toString());
@@ -53,16 +47,7 @@ public class testAPI {
 			System.out.println(term.toString());
 		}
 
-		/**AnnotationObject ann1 = new AnnotationObject("I1000", domain);
-		ann1.setAnnotationType("mention");
-		ann1.setCorpus("corpus1");
-		ann1.setDocumentID("doc1");
-		ArrayList<String> spanList = new ArrayList<String>();
-		spanList.add("1|10");
-		ann1.setSpan(spanList);
-		domain.setAnnotationToVariable(ann1, domain.getVariable("KA_1005"));**/
-
-		/**System.out.println("********** Modifier Map: **********");
+		System.out.println("********** Modifier Map: **********");
 		HashMap<String, ArrayList<Modifier>> modifierMap = domain.createModifierMap();
 		Iterator iterator = modifierMap.entrySet().iterator();
 		while (iterator.hasNext()){
@@ -73,7 +58,7 @@ public class testAPI {
 				System.out.print(modifier.getModName() + "  ");
 			}
 			System.out.println("");
-		}**/
+		}
 	}
 
 
