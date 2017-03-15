6fc29fae8c06d7252e12fd19159ec3ad89bbd6a7
leenlp <leenlp@q.com>
2017-03-15 17:16:07 -0600

    Latest.

---
+++ src/main/java/edu/utah/blulab/commandline/RestFulCommunication.java
@@ -0,0 +1,86 @@
+package edu.utah.blulab.commandline;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class RestFulCommunication {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+		// String url = args[0];
+		
+		// TEST
+		String url = "http://localhost:8080/MoonstoneServlet2/MoonstoneServlet2";
+		String text = "daughter helps the patient daily with chores";
+
+		RestFulCommunication http = new RestFulCommunication();
+		http.sendPost(url, text);
+	}
+
+	// HTTP GET request
+	private void sendGet(String url) throws Exception {
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		// add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		// print result
+		System.out.println(response.toString());
+
+	}
+
+	// HTTP POST request
+	private void sendPost(String url, String text) throws Exception {
+
+		URL obj = new URL(url);
+		java.net.HttpURLConnection con = (java.net.HttpURLConnection) obj.openConnection();
+		// add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		String urlParameters = "text=\"" + text + "\"";
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+		int responseCode = con.getResponseCode();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		System.out.println(response.toString());
+
+	}
+
+}
