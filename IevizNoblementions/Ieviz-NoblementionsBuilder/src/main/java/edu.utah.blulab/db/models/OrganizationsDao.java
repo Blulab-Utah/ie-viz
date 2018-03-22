9510484eb96de433b9902dff48ae041d15068c77
nagsubhadeep <subhadeep.nag@aggiemail.usu.edu>
2018-03-22 15:45:36 -0700

    User Metadata Persistence added

---
+++ IevizNoblementions/Ieviz-NoblementionsBuilder/src/main/java/edu.utah.blulab/db/models/OrganizationsDao.java
@@ -0,0 +1,46 @@
+package edu.utah.blulab.db.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Organizations", schema = "dbo", catalog = "NLP_DATASTORE")
+public class OrganizationsDao {
+    private int id;
+    private String organizationName;
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "organization_name", nullable = false, length = 255)
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrganizationsDao that = (OrganizationsDao) o;
+        return id == that.id &&
+                Objects.equals(organizationName, that.organizationName);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, organizationName);
+    }
+}
---
+++ IevizNoblementions/Ieviz-NoblementionsBuilder/src/main/java/edu.utah.blulab/db/models/UsersDao.java
@@ -0,0 +1,83 @@
+package edu.utah.blulab.db.models;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Users", schema = "dbo", catalog = "NLP_DATASTORE")
+public class UsersDao {
+    private int id;
+    private String username;
+    private int organizationId;
+    private Timestamp loginTime;
+    private Timestamp logoutTime;
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 255)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "organization_id", nullable = false)
+    public int getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(int organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    @Basic
+    @Column(name = "login_time", nullable = true)
+    public Timestamp getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Timestamp loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    @Basic
+    @Column(name = "logout_time", nullable = true)
+    public Timestamp getLogoutTime() {
+        return logoutTime;
+    }
+
+    public void setLogoutTime(Timestamp logoutTime) {
+        this.logoutTime = logoutTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersDao usersDao = (UsersDao) o;
+        return id == usersDao.id &&
+                organizationId == usersDao.organizationId &&
+                Objects.equals(username, usersDao.username) &&
+                Objects.equals(loginTime, usersDao.loginTime) &&
+                Objects.equals(logoutTime, usersDao.logoutTime);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, username, organizationId, loginTime, logoutTime);
+    }
+}
