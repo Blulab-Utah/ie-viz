f1eb83f53eb6ce9ee43b3a8eb4cdafa190843b9d
nagsubhadeep <subhadeep.nag@aggiemail.usu.edu>
2018-07-05 01:13:09 -0600

    IeViz restructured to process annotations, Schemas added

--- IevizNoblementions/IevizBuilder/src/main/java/edu.utah.blulab/containers/FeatureContainer.java
+++ IevizNoblementions/IevizBuilder/src/main/java/edu.utah.blulab/containers/FeatureContainer.java
@@ -2,40 +2,40 @@ package edu.utah.blulab.containers;
 
 public class FeatureContainer {
 
-    private String Property;
-    private String PropertyValue;
-    private String PropertyValueNumeric;
+    private String property;
+    private String propertyValue;
+    private String propertyValueNumeric;
 
     public String getProperty() {
-        return Property;
+        return property;
     }
 
     public void setProperty(String property) {
-        Property = property;
+        this.property = property;
     }
 
     public String getPropertyValue() {
-        return PropertyValue;
+        return propertyValue;
     }
 
     public void setPropertyValue(String propertyValue) {
-        PropertyValue = propertyValue;
+        this.propertyValue = propertyValue;
     }
 
     public String getPropertyValueNumeric() {
-        return PropertyValueNumeric;
+        return propertyValueNumeric;
     }
 
     public void setPropertyValueNumeric(String propertyValueNumeric) {
-        PropertyValueNumeric = propertyValueNumeric;
+        this.propertyValueNumeric = propertyValueNumeric;
     }
 
     @Override
     public String toString() {
         return "FeatureContainer{" +
-                "Property='" + Property + '\'' +
-                ", PropertyValue='" + PropertyValue + '\'' +
-                ", PropertyValueNumeric='" + PropertyValueNumeric + '\'' +
+                "property='" + property + '\'' +
+                ", propertyValue='" + propertyValue + '\'' +
+                ", propertyValueNumeric='" + propertyValueNumeric + '\'' +
                 '}';
     }
 }
